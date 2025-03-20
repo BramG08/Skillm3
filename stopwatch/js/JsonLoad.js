9a717711a2023856ad2cd4e46a5c2dbe5942d83e
BramG08 <bgrippeling08@gmail.com>
2025-03-20 10:50:20 +0100

    Finished opdracht 2

---
+++ stopwatch/js/JsonLoad.js
@@ -0,0 +1,15 @@
+
+
+ const text = document.getElementById("js-text");
+ const img = document.getElementById("js-img");
+
+    let data = fetch("../JsonFiles/gegevens.json").then(
+        function(binnenGekomenData){
+            return binnenGekomenData.json();
+        }).then(function(echteData){
+            console.log(echteData);
+            text.innerHTML = echteData.text;
+            img.src = echteData.img;
+        }
+    );
+    console.log(data);
